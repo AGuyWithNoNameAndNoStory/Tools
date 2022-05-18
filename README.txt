c7a4d70441e29b56aa6a79152c41de3196ecd952
AGuyWithNoNameAndNoStory <105793443+AGuyWithNoNameAndNoStory@users.noreply.github.com>
2022-05-18 20:56:39 +0800

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,6 @@
+This program is used to create a .cue file in specified folder.
+You should put cueCreater.exe in the folder of audio files where you want to create the playlist.
+Run the cueCreater.exe and enter the content as required.
+Then, you can get a .cue file.
+You can select any audios whatever you want when you run the .cue file.
+This program supports to extract .ape, .mp3 and.flac audio files.
---
+++ src.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <io.h>
+#include <string>
+using namespace std;
+#pragma warning(disable:4996)£»
+
+int main()
+{
+    int count = 0;
+    char Filename[100], Performer[50], Title[100];
+    printf("filename:");
+    gets_s(Filename);
+    char* str1 = (char*)malloc(100);
+    strcpy(str1, Filename);
+    strcat(str1, ".cue");
+    FILE *fpWrite = fopen(str1, "w"); //create the file
+
+    if (fpWrite == NULL) return 0;
+    // fixed format of .cue file
+    printf("performer:");
+    gets_s(Performer);
+    printf("title:");
+    gets_s(Title);
+    fprintf(fpWrite, "PERFORMER \"%s\"\n", Performer);
+    fprintf(fpWrite, "TITLE \"%s\"\n", Title);
+    long handle;  //create the handle
+    struct _finddata_t fileinfo; //struct of file information
+
+    for (int i = 0; i < 3; i++) {
+        const char* type[3] = { ".flac", ".mp3", ".ape" };  //the different type of audio files
+        const char* addr = "*";
+        char* str2 = (char*)malloc(1 + strlen(addr) + strlen(type[i]));
+        strcpy(str2, addr);
+        strcat(str2, type[i]);
+        const char* to_search = str2;        
+
+        handle = _findfirst(to_search, &fileinfo); //search the first audio files
+        if (-1 == handle) return -1;
+        // fixed format of .cue file
+        fprintf(fpWrite, "FILE \"%s\" WAVE\n", fileinfo.name);
+        string num;
+        if (++count < 10) num = '0' + to_string(count);
+        else num = to_string(count);
+        fprintf(fpWrite, "    TRACK %s AUDIO\n", num.c_str());
+        string name = fileinfo.name;
+        string title = name.substr(0, strlen(name.c_str()) - strlen(type[i]));
+        //printf("%s\n", title.c_str());
+        fprintf(fpWrite, "    TITLE \"%s\"\n", title.c_str());
+        fprintf(fpWrite, "    INDEX 01 00:00:00\n");
+    
+        //printf("%s\n", fileinfo.name); //print the file name
+        while (!_findnext(handle, &fileinfo)) //search other audio files
+        {
+            // fixed format of .cue file
+            fprintf(fpWrite, "FILE \"%s\" WAVE\n", fileinfo.name);
+            string num;
+            if (++count < 10) num = '0' + to_string(count);
+            else num = to_string(count);
+            fprintf(fpWrite, "    TRACK %s AUDIO\n", num.c_str());
+            string name = fileinfo.name;
+            string title = name.substr(0, strlen(name.c_str()) - strlen(type[i]));
+            fprintf(fpWrite, "    TITLE \"%s\"\n", title.c_str());
+            fprintf(fpWrite, "    INDEX 01 00:00:00\n");
+            //printf("%s\n", fileinfo.name); //print the file name
+        }
+    }
+    fclose(fpWrite); //save the file
+    _findclose(handle); //close the handle
+    return 0;
+}
+
+    
